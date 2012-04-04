cb05396fceb35748f176a6a77f3fdd21795a7c66
William Saults <wsaults@email.itt-tech.edu>
2012-04-03 20:44:14 -0500

    Adding the variables

--- sdi-project2/rename-me.js
+++ sdi-project2/rename-me.js
@@ -1,2 +1,15 @@
-alert("JavaScript works!");
-console.log("Making a change to commit.");
+// Author: William Saults
+// Date: 04/03/12
+// Assignment: Deliverable 2
+// Description: My Vacation - Retold.
+
+// Variables
+var areYourBagsPacked = false,
+	doYouNeedGas = true,
+	luck = 0,
+	isThereAWand = true
+	isThereCleaningSolution = true,
+	isThereTraffic = true,
+	theOverseersName = "'the unseen one'",
+	receptionistName = 'Ol\' McDonald';
+	isWindShieldDirty = true;
