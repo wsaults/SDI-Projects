b1845c1c80df71c34c3bccb9298aad49e5fb1bf6
William Saults <wsaults@email.itt-tech.edu>
2012-04-03 20:55:02 -0500

    Added the procedural function declaration

--- sdi-project2/rename-me.js
+++ sdi-project2/rename-me.js
@@ -7,17 +7,37 @@
 var areYourBagsPacked = false,
 	doYouNeedGas = true,
 	luck = 0,
-	isThereAWand = true
+	isThereAWand = true,
 	isThereCleaningSolution = true,
 	isThereTraffic = true,
 	theOverseersName = "'the unseen one'",
-	receptionistName = 'Ol\' McDonald';
-	isWindShieldDirty = true;
+	receptionistName = 'Ol\' McDonald',
+	isWindShieldDirty = true
+;
 	
 	
 // Procedure Declaration
+// Takes an argument
+var checkIfBagsArePacked = function (areYouPacked) {
+	// argument conditional
+	if (areYouPacked) {
+		// If true output something
+		console.log(theOverseersName.replace("t","T"),"is impressed that you've taken the initiative to pack your bags " + 
+		"\nand has bestowed +1 luck upon your voyage to come!");
+	luck++;
+	} else {
+		// if false output something else
+		console.log(theOverseersName.replace("t","T"),"is not too happy with you. You forgot to pack your bags! " + 
+		"Bummer and \n",theOverseersName.replace("t","T"),"reduces your luck by 1");
+	luck--;
+	}
+	// return
+	return;
+};
+
 
 // Boolean Function Declaration
+
 
 // Number Function Declaration
 
