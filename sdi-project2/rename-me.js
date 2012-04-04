88fb40a496f19733a1bb99ffee1b11994b9dfa92
William Saults <wsaults@email.itt-tech.edu>
2012-04-03 20:47:11 -0500

    Added comments to separate the important parts

--- sdi-project2/rename-me.js
+++ sdi-project2/rename-me.js
@@ -13,3 +13,30 @@ var areYourBagsPacked = false,
 	theOverseersName = "'the unseen one'",
 	receptionistName = 'Ol\' McDonald';
 	isWindShieldDirty = true;
+	
+	
+// Procedure Declaration
+
+// Boolean Function Declaration
+
+// Number Function Declaration
+
+// String Function Declaration
+
+// Array Function Declaration
+
+
+
+// Procedure Call
+
+// Boolean Function Call
+
+// Number Function Call
+
+// String Function Call
+
+// Array Function Call
+
+// Combine return values
+
+// Output return values
