1f318026f6b1cab32a0b0deb8ffb13c2044aecac
William Saults <wsaults@email.itt-tech.edu>
2012-04-17 20:19:18 -0500

    Added the nested loop, while loop, and math.

--- sdi-project3/saults_william_assignment3.js
+++ sdi-project3/saults_william_assignment3.js
@@ -28,7 +28,8 @@ var student = function (firstName, lastName, age, bool, array) {
 		"isMale": bool,
 		"classes": studentClasses,
 		"getClasses":getClasses,
-		"shoutName":shoutName
+		"shoutName":shoutName,
+		"addClass":addClass
 	};
 };
 
@@ -39,10 +40,11 @@ var luck = 0,
 
 // The story begins
 console.log("Hello everyone welcome back to class.");
-console.log("Lets get started!");
+console.log("It's a great day and the temperature is " + temperature + " degrees. Now lets get started!");
 console.log("Oh look a new student!");
 
 // Arguments: Boolean, Array, Number, String, Object
+// Returned values
 var newStudent = student ("Harley", "Quinn", 89, false, ["English","Spanish","History","Objective-C","JavaScript"]);
 
 
@@ -60,8 +62,17 @@ if(newStudent.firstName = "Harley") {
 	
 	// Nested Conditional
 	if(newStudent.age > 18) {
-		console.log("What!?! you're " + newStudent.age + " years old! Arn't you a little old for this class?\n" +
-		"Oh well, have a seat.");
+		console.log("What!?! you're " + newStudent.age + " years old! Arn't you a little old for this class?");
+		// Nested Loop
+		// While Loop
+		// Math
+		while (newStudent.age > 18) {
+			newStudent.age--;
+		}
+		
+		console.log("The instructor gives " + newStudent.firstName + " a peculiar potion. POOF!!!");
+		console.log("There your age is " + newStudent.age + ".");
+		console.log("Now you'll fit right in.");
 	} else {
 		console.log("Excellent, " + newStudent.age + " is a perfect age for this class.");
 	}
@@ -73,3 +84,7 @@ if(newStudent.firstName = "Harley") {
 console.log("So " + newStudent.firstName + " what classes are you in?");
 newStudent.getClasses();
 
+console.log("Oh well lets add Geometry to that!");
+newStudent.addClass("Geometry");
+console.log("Now your classes are:");
+newStudent.getClasses();
