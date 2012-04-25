9f777d2e3a5818e31ad006b74278be1ae1af4c81
William Saults <wsaults@email.itt-tech.edu>
2012-04-25 14:14:10 -0500

    Added find closest greater number function.

--- sdi-project4/saults_william_assignment4.js
+++ sdi-project4/saults_william_assignment4.js
@@ -27,42 +27,94 @@
 */
 
 // Suggestions:
-// UPC code testing
-// Word scramble
-// Character name generator params: (#ofNames from 1 - 3, male/female, race)
-// Password generator params: (#ofCharacters, strengthLV 1 - 3)
-// Uplifting comment generator
+// 13 UPC code testing
+// 14 Word scramble
+// 15 Character name generator params: (#ofNames from 1 - 3, male/female, race)
+// 16 Password generator params: (#ofCharacters, strengthLV 1 - 3)
 
 // My JS Lib
 var willsLib = function () {
 	// Private methods
-/* 	5    Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c". */
+	
+/* 	5    Given a string that is a list of things separated by a given string, as well as another string separator, 
+		 return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c". */
+/* ================================================================================================ */
 	var findAndReplaceDelimiter = function (str, delimiterStr, toDelimiterStr) {
-		
 		var re = new RegExp(delimiterStr,'g');
 		var result = str.replace(re,toDelimiterStr);
 		return result;
 	};
 	
-/*  6 	Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10 - params: (decimal, #ofPlaces) result = num.toFixed(2); */
+/*  6 	Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10 
+		- params: (decimal, #ofPlaces) result = num.toFixed(2); */
+/* ================================================================================================ */
 	var numberFormater = function (num, numberOfPlaces) {
 		var result = num.toFixed(numberOfPlaces);
 		return result;
 	}
 	
+/* 	10    Find the smallest value in an array that is greater than a given number params: array, numberToFind */
+/* ================================================================================================ */
+	var justALittleGreater = function (numArray, num) {
+	var result = 0,
+		difference = 0,
+		diffArray = []
+	;
+	
+	// How about a difference array where i grab the difference of each i - num and take the lowest positive number?
+	for (var i = 0; i < numArray.length; i++) {
+		difference += numArray[i];
+		diffArray.push(numArray[i] - num);
+/* 		console.log("DiffArray: " + diffArray[i]); */
+	}
+	
+	for (var j = 0; j < diffArray.length; j++) {
+		if (diffArray[j] > 0 && difference > diffArray[j]) {
+			difference = diffArray[j];
+			result = numArray[j];
+		}
+	}
+/* 	console.log("closest difference: " + difference); */
+	return result;
+	};
+	
+	
 	
 	// Public methods & properties
 	return {
 		"findAndReplaceDelimiter" : findAndReplaceDelimiter,
-		"numberFormater"  		  : numberFormater
+		"numberFormater"  		  : numberFormater,
+		"justALittleGreater"	  : justALittleGreater	
 	};
 };
 
-var lib = willsLib();
+
 /*
+is 3 greater than 20 
+no do nothing
+is 10 greater than 20
+no do nothing
+is 55 greater than 20
+yes result = 55
+is 36 greater than 20 but less than 55
+yes result = 36
+*/
+
+
+var lib = willsLib();
+
+/*
+// 5
+var findAndReplace = lib.findAndReplaceDelimiter("a,b,c", "," , "/");
+console.log(findAndReplace);
+*/
+
+/*
+// 6
 var formattedNumber = lib.numberFormater(2.1, 2);
 console.log(formattedNumber);
 */
 
-var findAndReplace = lib.findAndReplaceDelimiter("a,b,c", "," , "/");
-console.log(findAndReplace);
+// 10
+var numFinder = lib.justALittleGreater([3,10,55,36,19,21,22,1,9,77,99,54,33,7],20)
+console.log(numFinder + " Is the closest greater number.");
