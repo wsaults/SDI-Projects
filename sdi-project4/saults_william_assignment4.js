c99cf3ff936614646ba361b10e1f110bfe0dc28c
William Saults <wsaults@email.itt-tech.edu>
2012-04-25 11:18:30 -0500

    After a little Regex help from Dr.D the findAndReplace function now works properly.

--- sdi-project4/saults_william_assignment4.js
+++ sdi-project4/saults_william_assignment4.js
@@ -4,25 +4,25 @@
 // Description: My JS Lib
 
 /* String
-    Does a string follow a 123-456-7890 pattern like a phone number?
-    Does a string follow an aaa@bbb.ccc pattern like an email address?
-    Is the string a URL? (Does it start with http: or https:?)
-    Title-case a string (split into words, then uppercase the first letter of each word)
-    Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
+1    Does a string follow a 123-456-7890 pattern like a phone number?
+2    Does a string follow an aaa@bbb.ccc pattern like an email address?
+3    Is the string a URL? (Does it start with http: or https:?)
+4    Title-case a string (split into words, then uppercase the first letter of each word)
+5    Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
 
 */
 
 /* Number
-    Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10 - params: (decimal, #ofPlaces) result = num.toFixed(2);
-    Fuzzy-match a number: is the number above or below a number within a certain percent?
-    Find the number of hours or days difference between two dates.
-    Given a string version of a number such as "42", return the value as an actual Number, such as 42.
+6    Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10 - params: (decimal, #ofPlaces) result = num.toFixed(2);
+7    Fuzzy-match a number: is the number above or below a number within a certain percent?
+8    Find the number of hours or days difference between two dates.
+9    Given a string version of a number such as "42", return the value as an actual Number, such as 42.
 */
 
 /* Array
-    Find the smallest value in an array that is greater than a given number
-    Find the total value of just the numbers in an array, even if some of the items are not numbers.
-    Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
+10    Find the smallest value in an array that is greater than a given number
+11    Find the total value of just the numbers in an array, even if some of the items are not numbers.
+12    Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
 
 */
 
@@ -36,20 +36,33 @@
 // My JS Lib
 var willsLib = function () {
 	// Private methods
+/* 	5    Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c". */
+	var findAndReplaceDelimiter = function (str, delimiterStr, toDelimiterStr) {
+		
+		var re = new RegExp(delimiterStr,'g');
+		var result = str.replace(re,toDelimiterStr);
+		return result;
+	};
 	
-/* 	Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10 - params: (decimal, #ofPlaces) result = num.toFixed(2); */
-	var numberFormater = function (n, numberOfPlaces) {
-		var result = n.toFixed(numberOfPlaces);
+/*  6 	Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10 - params: (decimal, #ofPlaces) result = num.toFixed(2); */
+	var numberFormater = function (num, numberOfPlaces) {
+		var result = num.toFixed(numberOfPlaces);
 		return result;
 	}
 	
 	
 	// Public methods & properties
 	return {
-		"numberFormater"  : numberFormater
+		"findAndReplaceDelimiter" : findAndReplaceDelimiter,
+		"numberFormater"  		  : numberFormater
 	};
 };
 
 var lib = willsLib();
+/*
 var formattedNumber = lib.numberFormater(2.1, 2);
 console.log(formattedNumber);
+*/
+
+var findAndReplace = lib.findAndReplaceDelimiter("a,b,c", "," , "/");
+console.log(findAndReplace);
